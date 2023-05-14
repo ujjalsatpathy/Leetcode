e2746778b52916255c3548bde13c7e35f53f2754
Ujjal Satpathy <ujjalsatpathy@gmail.com>
2023-05-15 01:49:24 +0530

    merge sorted array

---
+++ agorithms/easy/sortd_array_merge.py
@@ -0,0 +1,15 @@
+# You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n,
+# representing the number of elements in nums1 and nums2 respectively.
+# Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+from typing import List
+
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        for i in range(0, n):
+            nums1[m + i] = nums2[i]
+
+        nums1.sort()

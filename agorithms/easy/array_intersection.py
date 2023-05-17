68abab1070687fae058be4b23e352b3209ae5eac
Ujjal Satpathy <ujjalsatpathy@gmail.com>
2023-05-18 00:19:08 +0530

    array intersection

---
+++ agorithms/easy/array_intersection.py
@@ -0,0 +1,17 @@
+# Given two integer arrays nums1 and nums2, return an array of their intersection.
+# Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
+from typing import List
+
+
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        arr = []
+        for i in nums1:
+            if nums2.__contains__(i):
+                nums2.remove(i)
+                arr.append(i)
+        return arr
+
+
+cls = Solution()
+print(cls.intersect([4,9,5], [9,4,9,8,4]))
